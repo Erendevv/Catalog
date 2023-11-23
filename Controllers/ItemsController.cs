630ab8e1d0860ad199f7d6f1b783c2e91f8bb9e1
Eren Uğur <m.erenugur37@gmail.com>
2023-11-23 15:18:45 +0300

    CRUD Operations added

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -37,5 +37,47 @@ namespace Catalog.Controllers{
            }
            return item.AsDto();
         }
+
+        // POST /items
+        [HttpPost]
+        public ActionResult<ItemDto> CreateItem(CreateItemDto createItemDto)
+        {
+            Item item = new(){
+                Id= Guid.NewGuid(),
+                Name = createItemDto.Name,
+                Price = createItemDto.Price,
+                CreatedDate = DateTimeOffset.UtcNow
+            };
+            repository.CreateItem(item);
+            return CreatedAtAction(nameof(GetItem), new {id = item.Id}, item.AsDto());
+        }
+
+        // PUT /items/1
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(Guid id, UpdateItemDto updateItemDto){
+            var existingItem = repository.GetItem(id);
+            if(existingItem is null){
+                return NotFound();
+            }
+            Item updatedItem = existingItem with {
+                Name = updateItemDto.Name,
+                Price = updateItemDto.Price
+            };
+
+            repository.UpdateItem(updatedItem);
+            return NoContent();
+
+        }
+
+        // DELETE /items/1
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id){ 
+         var existingItem = repository.GetItem(id);
+           if(existingItem is null){
+                return NotFound();
+            }
+            repository.DeleteItem(id);
+            return NoContent();
+        }
     }
 }
